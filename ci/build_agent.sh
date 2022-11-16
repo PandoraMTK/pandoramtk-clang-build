405ad3791689e9698261fe154417e16de1a92196
hamjin <335908093@qq.com>
2022-11-17 03:13:52 +0800

    build_agent: add sudo

--- ci/build_agent.sh
+++ ci/build_agent.sh
@@ -4,8 +4,8 @@ shopt -s nullglob
 set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y bison ca-certificates ccache clang cmake curl file flex gcc g++ git make ninja-build python3 texinfo zlib1g-dev libssl-dev libelf-dev patchelf
+sudo apt-get update
+sudo apt-get install -y bison ca-certificates ccache clang cmake curl file flex gcc g++ git make ninja-build python3 texinfo zlib1g-dev libssl-dev libelf-dev patchelf
 
 git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
